7e70f4856fdef797ed60b0cd4e7e633df3da823c
George G <kreach3r@users.noreply.github.com>
2016-04-20 17:13:45 +0300

    KrexusAbout: Add dynamic device maintainer (2/2)
    Device maintainers can now include their information in the "Krexus About" screen, by
    setting two build.prop values in the krexus_maintainer.mk file.
    This means no hardcoding and more freedom. :)
    
    This needs another commit in vendor.
    
    PRODUCT_PROPERTY_OVERRIDES += \
        ro.krexus.maintainer="your_name" \
        ro.krexus.maintainer.info="more_information_about_you_your_username_etc"
    
    Detailed instructions:
     - Make a file named "krexus_maintainer.mk" inside your device tree
     - Insert: http://hastebin.com/iluwatikeg.avrasm
     - Notice that you NEED to use underscores instead of space
     - Keep it simple and small because you may have max byte size errors
     - That's it

--- src/com/android/settings/krexus/KrexusAbout.java
+++ src/com/android/settings/krexus/KrexusAbout.java
@@ -2,6 +2,10 @@ package com.android.settings.krexus;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
 
 import com.android.internal.logging.MetricsLogger;
 
@@ -10,12 +14,35 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class KrexusAbout extends SettingsPreferenceFragment {
 
+    private static final String PROPERTY_KREXUS_MAINTAINER = "ro.krexus.maintainer";
+    private static final String PROPERTY_KREXUS_MAINTAINER_INFO = "ro.krexus.maintainer.info";
+    private static final String SCREEN_KREXUS_ABOUT = "screen_krexus_about";
+    private static final String CATEGORY_KREXUS_MAINTAINER = "category_krexus_about_maintainer";
+    private static final String KEY_KREXUS_MAINTAINER = "krexus_about_maintainer";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.krexus_about);
 	getActivity().getActionBar().setTitle("About Krexus");
+
+        final PreferenceScreen krexusAboutScreen = (PreferenceScreen)
+                findPreference(SCREEN_KREXUS_ABOUT);
+        final PreferenceGroup krexusMaintainerCategory = (PreferenceGroup)
+                findPreference(CATEGORY_KREXUS_MAINTAINER);
+        final Preference krexusMaintainerPref = (Preference)
+                findPreference(KEY_KREXUS_MAINTAINER);
+
+        if ((SystemProperties.get(PROPERTY_KREXUS_MAINTAINER).equals(""))
+                || (SystemProperties.get(PROPERTY_KREXUS_MAINTAINER_INFO).equals(""))) {
+            krexusAboutScreen.removePreference(krexusMaintainerCategory);
+        } else {
+            krexusMaintainerPref.setTitle(SystemProperties
+                    .get(PROPERTY_KREXUS_MAINTAINER).replaceAll("_"," "));
+            krexusMaintainerPref.setSummary(SystemProperties
+                    .get(PROPERTY_KREXUS_MAINTAINER_INFO).replaceAll("_"," "));
+        }
     }
 
     @Override
