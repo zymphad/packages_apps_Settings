b801d13b7ccae9787cbc9e6207c8aba3c390669b
George G <kreach3r@users.noreply.github.com>
2016-05-05 11:20:24 +0300

    About Phone: Expand Processor regex
    Some cpuinfo come with "MSM Hardware" instead of just "Hardware"
    Expand the search to reflect that

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -539,7 +539,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
             try {
                 while (null != (cpuinfo = reader.readLine())) {
-                    if (cpuinfo.startsWith("Hardware")) {
+                    if (cpuinfo.contains("Hardware")) {
                         Matcher m = Pattern.compile(PROC_HARDWARE_REGEX).matcher(cpuinfo);
                         if (m.matches()) {
                             return m.group(1);
