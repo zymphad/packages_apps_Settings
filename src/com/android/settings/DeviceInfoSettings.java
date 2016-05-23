3364b1efd231cb6611344d14ef592a7c9279c7a8
George G <kreach3r@users.noreply.github.com>
2016-05-23 19:43:08 +0300

    AboutPhone: decentralize processor Regex
    There is no correct Regex solution to this problem, as Google / CPU manufacturers and custom kernel developers don't follow any guidelines in regards to proc/cpuinfo.
    So we have hammerhead with "Hardware" line, clark with "MSM Hardware" line and flounder with "Processor" line.
    
    This commit:
     - De-hardcodes the processor regex string and moves it into krexus_strings
     - Makes it overlayable, per device (on build time or on runtime with Layers)

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -527,40 +527,25 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
     /**
      * Returns the Hardware or Processor value in /proc/cpuinfo,
-     * else returns "Unknown".
+     * else returns "Unknown" (regex can change with a device overlay)
      * @return a string that describes the processor
      */
-    private static String getDeviceProcessorInfo() {
+    private String getDeviceProcessorInfo() {
         // Hardware or Processor : XYZ
-        final String PROC_REGEX = ".*\\s*:\\s*(.*?)(?:\\(.*)?$"; /* hardware or processor string */
+        final String PROC_REGEX = getResources().getString(R.string.processor_regex); /* hardware or processor string */
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(FILENAME_PROC_CPUINFO));
             String cpuinfo;
-            String hardware = "";
-            String processor = "";
 
             try {
                 while (null != (cpuinfo = reader.readLine())) {
-                    if (cpuinfo.contains("Hardware")) {
-                        Matcher m = Pattern.compile(PROC_REGEX).matcher(cpuinfo);
-                        if (m.matches()) {
-                            hardware = m.group(1);
-                        }
-                    } else if (cpuinfo.contains("Processor")) {
-                        Matcher m = Pattern.compile(PROC_REGEX).matcher(cpuinfo);
-                        if (m.matches()) {
-                            processor = m.group(1);
-                        }
+                    Matcher m = Pattern.compile(PROC_REGEX).matcher(cpuinfo);
+                    if (m.matches()) {
+                        return m.group(1);
                     }
                 }
-                if (hardware.isEmpty() && processor.isEmpty()) {
-                    return "Unknown";
-                } else if (hardware.length() > processor.length()) {
-                    return hardware;
-                } else {
-                    return processor;
-                }
+                return "Unknown";
             } finally {
                 reader.close();
             }
